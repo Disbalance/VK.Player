454fbf19ae6bb6d261aee855137abc456048bc43
Роман Леденев <angelada_94@mail.ru>
2015-09-13 13:06:21 +0300

    Add Files

---
+++ VkPlayer/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace VkPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 190);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(317, 251);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(317, 123);
+            this.axWindowsMediaPlayer1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Случайное:ВЫКЛ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 462);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "VK.Player";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListBox listBox1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ VkPlayer/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Web;
+using Newtonsoft.Json.Linq;
+using System.Runtime.InteropServices;
+
+namespace VkPlayer
+{
+    public partial class Form1 : Form
+    {
+        public bool loop = false;
+        public bool random = false;
+        public List<Audio> audioList;
+        WMPLib.IWMPPlaylist PlayList;
+        WMPLib.IWMPMedia Media;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        internal static extern short GetAsyncKeyState(int vkey);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            MethodInvoker mi = new MethodInvoker(WaitKey);
+            mi.BeginInvoke(null, null);
+        }
+
+        private void WaitKey()
+        {
+            while (this.IsHandleCreated)
+            {
+                short res1 = GetAsyncKeyState(VK_HOME);
+                short res2 = GetAsyncKeyState(VK_END);
+                short res3 = GetAsyncKeyState(VK_NUM_PLUS);
+                short res4 = GetAsyncKeyState(VK_NUM_MINUS);
+
+                if (res1 != 0)
+                    axWindowsMediaPlayer1.Ctlcontrols.stop();
+                if (res2 != 0)
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                if (res3 != 0)
+                    axWindowsMediaPlayer1.Ctlcontrols.next();
+                if (res4 != 0)
+                    axWindowsMediaPlayer1.Ctlcontrols.previous();
+            }
+        }
+
+        public const int VK_NUM_PLUS = 0x6B;
+        public const int VK_NUM_MINUS = 0x6D;
+        public const int VK_HOME = 0x24;
+        public const int VK_END = 0x23;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            new Form2().Show();
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        public class Audio
+        {
+            public int aid { get; set; }
+            public int ower_id { get; set; }
+            public string artist { get; set; }
+            public string title { get; set; }
+            public int duration { get; set; }
+            public string url { get; set; }
+            public string lurics_id { get; set; }
+            public int genre { get; set; }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (!Settings1.Default.auth) { Thread.Sleep(500); }
+
+            WebRequest request =
+               WebRequest.Create("https://api.vk.com/method/audio.get?ower_id" + Settings1.Default.id + "&need_user=0&access_token=" + Settings1.Default.token);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+            responseFromServer = HttpUtility.HtmlDecode(responseFromServer);
+
+            JToken token = JToken.Parse(responseFromServer);
+            audioList = Enumerable.Skip(token["response"].Children(), 0).Select(c => c.ToObject<Audio>()).ToList();
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                PlayList = axWindowsMediaPlayer1.playlistCollection.newPlaylist("vkPlayList");
+                for (int i = 0; i < audioList.Count(); i++)
+                {
+                    Media = axWindowsMediaPlayer1.newMedia(audioList[i].url);
+                    PlayList.appendItem(Media);
+                    listBox1.Items.Add(audioList[i].artist + " - " + audioList[i].title);
+                }
+                axWindowsMediaPlayer1.currentPlaylist = PlayList;
+                axWindowsMediaPlayer1.settings.setMode("loop", false);
+                axWindowsMediaPlayer1.settings.setMode("shuffle", false);
+                axWindowsMediaPlayer1.Ctlcontrols.stop();
+            });
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                axWindowsMediaPlayer1.Ctlcontrols.currentItem = axWindowsMediaPlayer1.currentPlaylist.get_Item(listBox1.SelectedIndex);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (random)
+            {
+                random = false;
+                button2.Text = "Случайно:ВЫКЛ";
+                axWindowsMediaPlayer1.settings.setMode("shuffle", false);
+
+            }
+            else
+            {
+                random = true;
+                button2.Text = "Случайно:ВКЛ";
+                axWindowsMediaPlayer1.settings.setMode("shuffle", true);
+            }
+        }
+
+    }
+}
